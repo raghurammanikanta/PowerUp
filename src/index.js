0a3855f8d5f330f79ea76670120e23af4e2f01fd
raghurammanikanta <155514173+raghurammanikanta@users.noreply.github.com>
2025-06-17 18:34:48 +0530

    Theme provider added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +12,11 @@ root.render(
     <BrowserRouter  future={{
     v7_relativeSplatPath: true,v7_startTransition: true,
   }}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
     </BrowserRouter>
+
   </React.StrictMode>
 );
 
